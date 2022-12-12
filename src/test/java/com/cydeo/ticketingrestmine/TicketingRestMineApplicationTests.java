a34ae91f6b1b410a0453ca5f0b215b3bd2cd632f
jawidkarimi <104651569+jawidkarimi@users.noreply.github.com>
2022-12-12 01:05:03 -0800

    Initial commit

---
+++ src/test/java/com/cydeo/ticketingrestmine/TicketingRestMineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cydeo.ticketingrestmine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketingRestMineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
